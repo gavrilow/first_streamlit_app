101b31688ab48f233fe560e0b256c2b27a4b4c06
Dmytro Havrilov <gavrilow@gmail.com>
2022-07-06 14:12:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 #snowflake application for 2nd bage
 import streamlit
 streamlit.title('My healthy restaurant application')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
