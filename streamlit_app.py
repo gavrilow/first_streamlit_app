f6d874957bf666f5ae58aca397021f49449ba9a9
Dmytro Havrilov <gavrilow@gmail.com>
2022-07-07 19:05:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,3 +30,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response.json())
 
 
+# normalize json with pandas
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output normalized json in table
+streamlit.dataframe(fruityvice_normalized)
