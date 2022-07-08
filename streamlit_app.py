6a58e553793e4358e373e038381eb5f0ba0fc7df
Dmytro Havrilov <gavrilow@gmail.com>
2022-07-08 18:15:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,13 +25,9 @@ streamlit.dataframe(fruits_to_show)
 
 import requests
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# add_my_fruit = streamlit.text_input('Add my fruit?','jackfruit')
-# streamlit.write('The user entered ', add_my_fruit)
-# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
 
 # normalize json with pandas
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
@@ -47,3 +43,6 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+add_my_fruit = streamlit.text_input('Add my fruit?','jackfruit')
+streamlit.write('The user entered ', add_my_fruit)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
