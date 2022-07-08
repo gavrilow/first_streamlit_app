1329c9dd9dc943153e64697d2c87893afa4d75cc
Dmytro Havrilov <gavrilow@gmail.com>
2022-07-08 16:54:45 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 
 add_my_fruit = streamlit.text_input('Add my fruit?','Jackfruit')
 streamlit.write('The user entered ', add_my_fruit)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
 
 # normalize json with pandas
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
