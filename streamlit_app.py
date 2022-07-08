c64d4a7755efa0e25c4a2757ee0fda2dbf1bd930
Dmytro Havrilov <gavrilow@gmail.com>
2022-07-08 16:57:40 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,18 +29,14 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-
-add_my_fruit = streamlit.text_input('Add my fruit?','jackfruit')
-streamlit.write('The user entered ', add_my_fruit)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
+# add_my_fruit = streamlit.text_input('Add my fruit?','jackfruit')
+# streamlit.write('The user entered ', add_my_fruit)
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
 
 # normalize json with pandas
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output normalized json in table
 streamlit.dataframe(fruityvice_normalized)
-
-
-
 
 import snowflake.connector
 
